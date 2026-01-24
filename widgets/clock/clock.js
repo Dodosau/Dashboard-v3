9497dd1dd31206967893fd5b2462699d548e7018
Dodosau <dorian.sauzede@gmail.com>
2026-01-24 05:58:51 -0500

    Update clock.js

--- widgets/clock/clock.js
+++ widgets/clock/clock.js
@@ -1,10 +1,12 @@
+(function () {
+  function tick() {
+    var d = new Date();
+    var h = ("0" + d.getHours()).slice(-2);
+    var m = ("0" + d.getMinutes()).slice(-2);
 
-(function(){
-function tick(){
- var d=new Date();
- document.getElementById("clockTime").innerHTML=
-  ("0"+d.getHours()).slice(-2)+":"+("0"+d.getMinutes()).slice(-2);
- document.getElementById("clockDate").innerHTML=d.toDateString();
-}
-tick(); setInterval(tick,10000);
+    document.getElementById("clockTime").textContent = h + ":" + m;
+  }
+
+  tick();
+  setInterval(tick, 10000); // MAJ toutes les 10s
 })();
